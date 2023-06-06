ef279fff676710567a6dcd0b40795346e8cacaad
Jared Valenzuela <jared.valenzuela@epn.edu.ec>
2023-06-06 07:40:16 -0500

    Constructores, getters and setters

---
+++ src/Geometria.java
@@ -0,0 +1,38 @@
+public class Geometria {
+    int nLados;//numero lados
+
+    public Geometria(int nLados, float lado) {
+        this.nLados = nLados;
+        this.lado = lado;
+    }
+
+    public float getLado() {
+        return lado;
+    }
+
+    public void setLado(float lado) {
+        this.lado = lado;
+    }
+
+    float lado;//longuitud de lados
+
+    public Geometria(int nLados) {//constructor de NLados
+        this.nLados = nLados;
+    }
+
+    public int getnLados() {
+        return nLados;
+    }
+
+    public void setnLados(int nLados) {
+        this.nLados = nLados;
+    }
+
+    public float areaCuadrado(){
+
+    }
+    public float perimetroCuadrado(){
+
+    }
+
+}
